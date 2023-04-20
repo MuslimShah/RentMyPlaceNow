4aac14b529c2bc543fed8be47253d2d48e4faf9c
Muslim shah <smuslimshah7@gmail.com>
2023-04-20 18:11:26 +0500

    pushig starter files

---
+++ errors/index.js
@@ -0,0 +1,8 @@
+//import all custom errors here
+const CustomAPIError = require('./custom-api-error');
+const BadRequest = require('./bad-request');
+
+module.exports = {
+    CustomAPIError,
+    BadRequest
+}
